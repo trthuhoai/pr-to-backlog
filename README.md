c7e84af815bb4fd2b5a67d4deff87be60f006b0e
kozocom-hoaittt <hoaittt@kozo-japan.com>
2023-02-10 13:19:57 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Integrate Github to Backlog
---
+++ src/main.ts
@@ -0,0 +1,59 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import Webhooks from "@octokit/webhooks";
+import axios from "axios";
+import querystring from "querystring";
+
+async function run() {
+  try {
+    const host = core.getInput("backlog-host", { required: true });
+    const apiKey = core.getInput("api-key", { required: true });
+
+    if (github.context.eventName != "pull_request") {
+      core.error("Event should be a pull_request");
+    }
+    const pr = github.context.payload as Webhooks.WebhookPayloadPullRequest;
+    const title = pr.pull_request.title;
+
+    let payload;
+    switch (pr.action) {
+      case "opened":
+        payload = {
+          comment: `A Pull request [#${pr.number}](${pr.pull_request.html_url}) created by ${pr.pull_request}: ${title}`,
+        };
+        break;
+      case "closed":
+        if (pr.pull_request.merged) {
+          payload = {
+            // statusId: 3,
+            comment: `A Pull request [#${pr.number}](${pr.pull_request.html_url}) merged and closed by ${pr.pull_request}: ${title}`,
+          };
+        } else {
+          payload = {
+            comment: `A Pull request [#${pr.number}](${pr.pull_request.html_url}) closed by ${pr.pull_request}: ${title}`,
+          };
+        }
+        break;
+      default:
+        console.log(`Unsupported action: ${pr.action}`);
+        return;
+    }
+
+    for (const issueKey of parseIssueKey(title)) {
+      const apiUrl = `https://${host}/api/v2/issues/${issueKey}?apiKey=${apiKey}`;
+      await axios.patch(apiUrl, querystring.stringify(payload), {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      });
+    }
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+function parseIssueKey(title: string): Array<string> {
+  let re = /([A-Z][A-Z0-9_]+-\d+)/g;
+  return title.match(re) || [];
+}
+
+run();
